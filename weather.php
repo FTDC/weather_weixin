439cf2a80b234f68b547bafc087921290c55acda
xjin <mrtop@live.cn>
2016-05-01 00:18:46 +0800

    最新文件

--- weather.php
+++ weather.php
@@ -17,15 +17,17 @@ define('SITE_PATH', getcwd() . '/');
 //项目路径
 define('PROJECT_PATH', SITE_PATH . 'shuipf/');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG', true);
+define('APP_DEBUG', false);
 // 应用公共目录
 define('COMMON_PATH', PROJECT_PATH . 'Common/');
 // 定义应用目录
 define('APP_PATH', PROJECT_PATH . 'Application/');
+define('BIND_MODULE',  'Api');
+define('BIND_CONTROLLER',  'Index');
+define('BIND_ACTION',  'index');
 //应用运行缓存目录
 define("RUNTIME_PATH", SITE_PATH . "#runtime/");
-//模板存放路径
-define('TEMPLATE_PATH', PROJECT_PATH . 'Template/');
 
+// echo  PROJECT_PATH;  exit();
 // 引入ThinkPHP入口文件
 require PROJECT_PATH . 'Core/ThinkPHP.php';
