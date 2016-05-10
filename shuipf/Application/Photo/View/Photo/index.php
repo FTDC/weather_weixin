d21fff9e3a0c53fd0c8a378a7ed5910366ca972e
Administrator <mrtop@live.cn>
2016-05-11 00:55:05 +0800

    微信界面的修改， 上传功能未实现

--- shuipf/Application/Photo/View/Photo/index.php
+++ shuipf/Application/Photo/View/Photo/index.php
@@ -329,13 +329,14 @@
                 success: function (res) {
                     var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
                     var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-                    $.ajax({
+                    $.ajax(
+                        {
                             type: "post",
                             url: '<?php echo U('Photo/Photo/getaddressbylngb'); ?>',
                             dataType: "json",
                             data: {'latitude': latitude, 'longitude': longitude},
                             success: function (data) {
-                                alert(data)
+                                alert(JSON.stringify(data))
                             }
                         })
                 },
